852df5ed7c8a9558f0aff71f40df52a2fe13fee3
Giulio <gformenti@rockefeller.edu>
2020-07-20 18:18:51 -0400

    added CLR extractor

---
+++ trio/extract.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PICARD_PATH=/rugpfs/fs0/vgl/store/gformenti/bin/
+
+BAM_NAME=$(sed -n "${SLURM_ARRAY_TASK_ID}p" bam.uniq)
+
+BAM_FILE=$(grep ${BAM_NAME} bam.ls)
+
+if ! [[ -e "filtered/${BAM_NAME}.subreads.bam" ]]; then
+
+	java -jar ${PICARD_PATH}/picard.jar FilterSamReads I=${BAM_FILE} O=filtered/${BAM_NAME}.subreads.bam READ_LIST_FILE=reads/${BAM_NAME}.ls VALIDATION_STRINGENCY=LENIENT FILTER=includeReadList
+	
+fi
