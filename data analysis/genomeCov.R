0532bc83824df5d9eca0a4d3aab7feb29d29de4a
Giulio <gformenti@rockefeller.edu>
2020-12-07 17:32:09 -0500

    genomecov

---
+++ data analysis/genomeCov.R
@@ -0,0 +1,40 @@
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+
+library(ggplot2)
+library(dplyr)
+library(grid)
+library(gridExtra)
+
+data <- read.csv("chrY.genomecov", header = TRUE, sep = "\t", na.strings="NA")
+
+data2 <- read.csv("noAutosomes.noX.extract.genomecov", header = FALSE, col.names = c("scaffold","cov","count", "len", "freq"), sep = "\t", na.strings="NA")
+
+data2 %>% filter (len>52500) %>% filter (len<55000) %>% ggplot(aes(x = cov, y = count)) + 
+  geom_line(aes(color = scaffold)) +
+  xlim(c(0,100))
+
+p<-ggplot(data2, aes(x = cov, y = count)) + 
+  geom_line(size=5) +
+  xlim(c(0,100)) +
+  geom_vline(aes(xintercept=30), color="black", linetype="dashed", size=3) +
+  geom_vline(aes(xintercept=60), color="black", linetype="dashed", size=3) +
+  theme(
+    axis.title = element_text(family = "Arial",
+                              size = rel(5), colour = "black"), 
+    axis.text = element_text(family = "Arial",
+                             size = rel(5), colour = "black"),
+    axis.ticks.length = unit(2, "cm"),
+    axis.ticks = element_line(colour = "black", size=(1)),
+    plot.title = element_text(size = rel(5), face = "bold")
+  )
+
+plots = data2 %>% 
+    group_by(scaffold) %>% 
+    do(plots = p %+% .) %>% 
+    rowwise() %>%
+    do(x=.$plots + ggtitle(.$scaffold))
+
+png("marmoset scaffolds.png", width = 20000, height = 20000) 
+grid.arrange(grobs = plots$x, ncol = 18)
+dev.off()
+  
