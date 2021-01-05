05406a101ed6db0a9867a041084bae99d86b97ac
Giulio <gformenti@rockefeller.edu>
2021-01-05 10:50:09 -0500

    sda

---
+++ marmoset Y/SDA.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/rugpfs/fs0/vgl/store/gformenti/bin/SDA/SDA collapse --reads $2 --coverage $3 --ref $1 --platform subread --debug -d $4 $5 $6
---
+++ marmoset Y/SDAdenovo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/rugpfs/fs0/vgl/store/gformenti/bin/SDA/SDA denovo --input noAutosomes.noX.bam --threads 24 --debug -d noAutosomes.noX.denovo --ref GCA_011100555.1_mCalJac1.pat.X_genomic.reorderedY.fasta
